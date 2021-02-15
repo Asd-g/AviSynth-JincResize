218da816ee442c8fde220f22162eba4463b7e9c2
DTL2020 <68707763+DTL2020@users.noreply.github.com>
2021-02-15 19:06:41 +0300

    Added opt=0 _cb_mt
    Added first version of internally multithreaded cb_mt for C code now. Tested with 2 threads only.

---
+++ KernelRow_avx512.cpp
@@ -0,0 +1,232 @@
+#include "JincRessize.h"
+
+#if !defined(__AVX512F__) && !defined(__INTEL_COMPILER)
+void JincResize::KernelRowAll_avx512_mul(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride)
+{} // do nothing
+
+void JincResize::KernelRowAll_avx512_mul_cb(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride)
+{} // do nothing
+#else
+
+void JincResize::KernelRowAll_avx512_mul(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride)
+{
+    // current input plane sizes
+    iWidthEl = iInpWidth + 2 * iKernelSize;
+    iHeightEl = iInpHeight + 2 * iKernelSize;
+
+    float* pfCurrKernel = g_pfKernel;
+
+    memset(g_pfFilteredImageBuffer, 0, iWidthEl * iHeightEl * iMul * iMul);
+    const int k_col16 = iKernelSize - (iKernelSize % 16);
+
+#pragma omp parallel for num_threads(threads_) 
+    for (int64_t row = iTaps; row < iHeightEl - iTaps; row++) // input lines counter
+    {
+        // start all row-only dependent ptrs here
+        int64_t iProcPtrRowStart = (row * iMul - (iKernelSize / 2)) * iWidthEl * iMul - (iKernelSize / 2);
+
+        // prepare float32 pre-converted row data for each threal separately
+        int64_t tidx = omp_get_thread_num();
+        float* pfInpRowSamplesFloatBufStart = pfInpFloatRow + tidx * iWidthEl;
+//        (this->*GetInpElRowAsFloat)(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart);
+        GetInpElRowAsFloat_avx2(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart); // still no avx512
+
+        for (int64_t col = iTaps; col < iWidthEl - iTaps; col++) // input cols counter
+        {
+            float* pfCurrKernel_pos = g_pfKernel;
+
+            float* pfProc = g_pfFilteredImageBuffer + iProcPtrRowStart + col * iMul;
+
+            for (int64_t k_row = 0; k_row < iKernelSize; k_row++)
+            {
+                for (int64_t k_col = 0; k_col < k_col16; k_col += 16)
+                {
+                    *(__m512*)(pfProc + k_col) = _mm512_fmadd_ps(*(__m512*)(pfCurrKernel_pos + k_col), _mm512_broadcast_f32x4(_mm_load_ps1(pfInpRowSamplesFloatBufStart + col)), *(__m512*)(pfProc + k_col));
+                }
+
+                // need to process last up to 15 floats separately..
+                for (int64_t k_col = k_col16; k_col < iKernelSize; ++k_col)
+                    pfProc[k_col] += pfCurrKernel_pos[k_col];
+
+                pfProc += iWidthEl * iMul; // point to next start point in output buffer now
+                pfCurrKernel_pos += iKernelSize; // point to next kernel row now
+            } // k_row
+        } // col
+    }
+
+    ConvertToInt_avx2(iInpWidth, iInpHeight, dst, iDstStride);
+}
+
+void JincResize::KernelRowAll_avx512_mul_cb(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride)
+{
+    // current input plane sizes
+    iWidthEl = iInpWidth + 2 * iKernelSize;
+    iHeightEl = iInpHeight + 2 * iKernelSize;
+
+    float* pfCurrKernel = g_pfKernel;
+
+    memset(pfFilteredCirculatingBuf, 0, iWidthEl * iKernelSize * iMul * sizeof(float));
+
+    const int k_col16 = iKernelSize - (iKernelSize % 16);
+
+    for (int64_t row = iTaps; row < iHeightEl - iTaps; row++) // input lines counter
+    {
+        // start all row-only dependent ptrs here
+        // prepare float32 pre-converted row data for each threal separately
+        //int64_t tidx = omp_get_thread_num();
+        float* pfInpRowSamplesFloatBufStart = pfInpFloatRow; // +tidx * iWidthEl;
+//        (this->*GetInpElRowAsFloat)(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart);
+        GetInpElRowAsFloat_avx2(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart); // still no avx512
+
+        for (int64_t col = iTaps; col < iWidthEl - iTaps; col++) // input cols counter
+        {
+            float* pfCurrKernel_pos = pfCurrKernel;
+            float* pfProc;
+
+            for (int64_t k_row = 0; k_row < iKernelSize; k_row++)
+            {
+                pfProc = vpfRowsPointers[k_row] + col * iMul;
+                for (int64_t k_col = 0; k_col < k_col16; k_col += 16)
+                {
+                    *(__m512*)(pfProc + k_col) = _mm512_fmadd_ps(*(__m512*)(pfCurrKernel_pos + k_col), _mm512_broadcast_f32x4(_mm_load_ps1(pfInpRowSamplesFloatBufStart + col)), *(__m512*)(pfProc + k_col));
+                }
+
+                // need to process last up to 15 floats separately..
+                for (int64_t k_col = k_col16; k_col < iKernelSize; ++k_col)
+                    pfProc[k_col] += pfCurrKernel_pos[k_col];
+
+                pfCurrKernel_pos += iKernelSize; // point to next kernel row now
+            } // k_row
+        } // col
+
+        int iOutStartRow = (row - (iTaps + iKernelSize)) * iMul;
+        //iMul rows ready - output result, skip iKernelSize+iTaps rows from beginning
+        if (iOutStartRow >= 0 && iOutStartRow < (iInpHeight)*iMul)
+        {
+            ConvertiMulRowsToInt_avx2(iInpWidth, iOutStartRow, dst, iDstStride);
+        }
+
+        // circulate pointers to iMul rows upper
+        std::rotate(vpfRowsPointers.begin(), vpfRowsPointers.begin() + iMul, vpfRowsPointers.end());
+
+        // clear last iMul rows
+        for (int i = iKernelSize - iMul; i < iKernelSize; i++)
+        {
+            memset(vpfRowsPointers[i], 0, iWidthEl * iMul * sizeof(float));
+        }
+    } // row
+}
+
+__forceinline void JincResize::ConvertiMulRowsToInt_avx2(int iInpWidth, int iOutStartRow, unsigned char* dst, int iDstStride) // still no avx512 version
+{
+    const int col32 = (iInpWidth * iMul) - ((iInpWidth * iMul) % 32);
+
+    __m256 my_zero_ymm2;
+
+    int row_float_buf_index = 0;
+
+    for (int64_t row = iOutStartRow; row < iOutStartRow + iMul; row++)
+    {
+        my_zero_ymm2 = _mm256_setzero_ps();
+
+        int64_t col = 0;
+        float* pfProc = vpfRowsPointers[row_float_buf_index] + (iKernelSize + iTaps) * iMul;
+        unsigned char* pucDst = dst + row * (int64_t)iDstStride;
+
+        for (col = 0; col < col32; col += 32)
+        {
+            __m256 my_Val_ymm1 = _mm256_load_ps(pfProc);
+            __m256 my_Val_ymm2 = _mm256_load_ps(pfProc + 8);
+            __m256 my_Val_ymm3 = _mm256_load_ps(pfProc + 16);
+            __m256 my_Val_ymm4 = _mm256_load_ps(pfProc + 24);
+
+            my_Val_ymm1 = _mm256_max_ps(my_Val_ymm1, my_zero_ymm2);
+            my_Val_ymm2 = _mm256_max_ps(my_Val_ymm2, my_zero_ymm2);
+            my_Val_ymm3 = _mm256_max_ps(my_Val_ymm3, my_zero_ymm2);
+            my_Val_ymm4 = _mm256_max_ps(my_Val_ymm4, my_zero_ymm2);
+
+            __m256i my_iVal_ymm1 = _mm256_cvtps_epi32(my_Val_ymm1);
+            __m256i my_iVal_ymm2 = _mm256_cvtps_epi32(my_Val_ymm2);
+            __m256i my_iVal_ymm3 = _mm256_cvtps_epi32(my_Val_ymm3);
+            __m256i my_iVal_ymm4 = _mm256_cvtps_epi32(my_Val_ymm4);
+
+            __m256i my_iVal_12 = _mm256_packus_epi32(my_iVal_ymm1, my_iVal_ymm2);
+            __m256i my_iVal_34 = _mm256_packus_epi32(my_iVal_ymm3, my_iVal_ymm4);
+
+            __m256i my_iVal_1234 = _mm256_packus_epi16(my_iVal_12, my_iVal_34);
+
+            __m256i my_iVal_ymm1234 = _mm256_permutevar8x32_epi32(my_iVal_1234, _mm256_setr_epi32(0, 4, 1, 5, 2, 6, 3, 7));
+
+            _mm256_stream_si256((__m256i*)(pucDst), my_iVal_ymm1234);
+
+            pucDst += 32;
+            pfProc += 32;
+        }
+        // last up to 31..
+        for (int64_t l_col = col32; l_col < iInpWidth * iMul; ++l_col)
+        {
+
+            float fVal = *pfProc;
+
+            fVal += 0.5f;
+
+            if (fVal > 255.0f)
+            {
+                fVal = 255.0f;
+            }
+            if (fVal < 0.0f)
+            {
+                fVal = 0.0f;
+            }
+
+            *pucDst = (unsigned char)fVal;
+
+            pucDst++;
+            pfProc++;
+        } //l_col
+
+        row_float_buf_index++;
+    } // row
+}
+
+__forceinline void JincResize::GetInpElRowAsFloat_avx2(int iInpRow, int iCurrInpHeight, int iCurrInpWidth, unsigned char* pCurr_src, int iCurrSrcStride, float* dst)
+{
+    int64_t col;
+
+    const int64_t col8 = iKernelSize - iKernelSize % 8;
+    // row range from iTaps to iHeightEl - iTaps - 1
+    // use iWidthEl and iHeightEl set in KernelRow()
+
+    if (iInpRow < iKernelSize) iInpRow = iKernelSize;
+    if (iInpRow > (iCurrInpHeight + iKernelSize - 1)) iInpRow = iCurrInpHeight + iKernelSize - 1;
+
+    unsigned char* pCurrRowSrc = pCurr_src + (iInpRow - iKernelSize) * iCurrSrcStride;
+
+    // start cols
+    __m256 my_ymm_start = _mm256_setzero_ps();
+    my_ymm_start = _mm256_broadcastss_ps(_mm_cvt_si2ss(_mm256_castps256_ps128(my_ymm_start), *pCurrRowSrc));
+
+    for (col = iKernelSize - 8; col >= 0; col -= 8) // left
+    {
+        _mm256_store_ps(dst + col, my_ymm_start);
+    }
+
+    // mid cols
+    unsigned char* pCurrColSrc = pCurrRowSrc;
+    for (col = iKernelSize; col < iKernelSize + iCurrInpWidth; col += 8)
+    {
+        __m256 src_ps = _mm256_cvtepi32_ps(_mm256_cvtepu8_epi32(_mm_loadu_si128((__m128i*)pCurrColSrc)));
+        _mm256_store_ps(dst + col, src_ps);
+        pCurrColSrc += 8;
+    }
+
+    // end cols
+    __m256 my_ymm_end = _mm256_setzero_ps();
+    my_ymm_end = _mm256_broadcastss_ps(_mm_cvt_si2ss(_mm256_castps256_ps128(my_ymm_start), pCurrRowSrc[iCurrInpWidth - 1]));
+    for (col = iKernelSize + iCurrInpWidth; col < iWidthEl; col += 8) // right
+    {
+        _mm256_store_ps(dst + col, my_ymm_end);
+    }
+
+}
+#endif
---
+++ resize_plane_sse41.cpp
@@ -0,0 +1,108 @@
+#include "JincRessize.h"
+
+template <typename T>
+void JincResize::resize_plane_sse41(EWAPixelCoeff* coeff[3], PVideoFrame& src, PVideoFrame& dst, IScriptEnvironment* env)
+{
+    const int pixel_size = sizeof(T);
+    const int planes_y[4] = { PLANAR_Y, PLANAR_U, PLANAR_V, PLANAR_A };
+    const int planes_r[4] = { PLANAR_G, PLANAR_B, PLANAR_R, PLANAR_A };
+    const int* current_planes = (vi.IsYUV() || vi.IsYUVA()) ? planes_y : planes_r;
+    for (int i = 0; i < planecount; ++i)
+    {
+        const int plane = current_planes[i];
+
+        const int src_stride = src->GetPitch(plane) / pixel_size;
+        const int dst_stride = dst->GetPitch(plane) / pixel_size;
+        const int dst_width = dst->GetRowSize(plane) / pixel_size;
+        const int dst_height = dst->GetHeight(plane);
+        const T* srcp = reinterpret_cast<const T*>(src->GetReadPtr(plane));
+        const __m128 min_val = (i && !vi.IsRGB()) ? _mm_set_ps1(-0.5f) : _mm_setzero_ps();
+
+        if (bAddProc)
+        {
+            const int src_width = src->GetRowSize(plane) / pixel_size;
+            const int src_height = src->GetHeight(plane);
+            unsigned char* dstp_add = reinterpret_cast<unsigned char*>(dst->GetWritePtr(plane));
+			(this->*KernelProcAll)((unsigned char*)srcp, src_stride, src_width, src_height, (unsigned char*)dstp_add, dst_stride);
+            continue;
+        }
+
+#pragma omp parallel for num_threads(threads_)
+        for (int y = 0; y < dst_height; ++y)
+        {
+            T* __restrict dstp = reinterpret_cast<T*>(dst->GetWritePtr(plane)) + static_cast<int64_t>(y) * dst_stride;
+
+            for (int x = 0; x < dst_width; ++x)
+            {
+                EWAPixelCoeffMeta* meta = coeff[i]->meta + static_cast<int64_t>(y) * dst_width + x;
+                const T* src_ptr = srcp + (meta->start_y * static_cast<int64_t>(src_stride)) + meta->start_x;
+                const float* coeff_ptr = coeff[i]->factor + meta->coeff_meta;
+                __m128 result = _mm_setzero_ps();
+
+//                if constexpr (std::is_same_v<T, uint8_t>)
+				if (std::is_same_v<T, uint8_t>)
+                {
+                    for (int ly = 0; ly < coeff[i]->filter_size; ++ly)
+                    {
+                        for (int lx = 0; lx < coeff[i]->filter_size; lx += 4)
+                        {
+                            const __m128 src_ps = _mm_cvtepi32_ps(_mm_cvtepu8_epi32(_mm_cvtsi32_si128(*(reinterpret_cast<const int32_t*>(src_ptr + lx)))));
+                            const __m128 coeff = _mm_load_ps(coeff_ptr + lx);
+                            result = _mm_add_ps(result, _mm_mul_ps(src_ps, coeff));
+                        }
+
+                        coeff_ptr += coeff[i]->coeff_stride;
+                        src_ptr += src_stride;
+                    }
+
+                    const __m128 hsum = _mm_hadd_ps(_mm_hadd_ps(result, result), _mm_hadd_ps(result, result));
+                    const __m128i src_int = _mm_packus_epi16(_mm_packs_epi32(_mm_cvtps_epi32(hsum), _mm_setzero_si128()), _mm_setzero_si128());
+                    _mm_storeu_si128(reinterpret_cast<__m128i*>(dstp + x), src_int);
+                }
+//                else if constexpr (std::is_same_v<T, uint16_t>)
+				else if (std::is_same_v<T, uint16_t>)
+                {
+                    for (int ly = 0; ly < coeff[i]->filter_size; ++ly)
+                    {
+                        for (int lx = 0; lx < coeff[i]->filter_size; lx += 4)
+                        {
+                            const __m128 src_ps = _mm_cvtepi32_ps(_mm_cvtepu16_epi32(_mm_loadu_si128(reinterpret_cast<const __m128i*>(src_ptr + lx))));
+                            const __m128 coeff = _mm_load_ps(coeff_ptr + lx);
+                            result = _mm_add_ps(result, _mm_mul_ps(src_ps, coeff));
+                        }
+
+                        coeff_ptr += coeff[i]->coeff_stride;
+                        src_ptr += src_stride;
+                    }
+
+                    const __m128 hsum = _mm_hadd_ps(_mm_hadd_ps(result, result), _mm_hadd_ps(result, result));
+                    const __m128i src_int = _mm_packus_epi32(_mm_cvtps_epi32(hsum), _mm_setzero_si128());
+                    _mm_storeu_si128(reinterpret_cast<__m128i*>(dstp + x), src_int);
+
+                }
+                else
+                {
+                    for (int ly = 0; ly < coeff[i]->filter_size; ++ly)
+                    {
+                        for (int lx = 0; lx < coeff[i]->filter_size; lx += 4)
+                        {
+//                            const __m128 src_ps = _mm_max_ps(_mm_loadu_ps(src_ptr + lx), min_val);
+							const __m128 src_ps = _mm_max_ps(*(__m128*)(src_ptr + lx), min_val);
+                            const __m128 coeff = _mm_load_ps(coeff_ptr + lx);
+                            result = _mm_add_ps(result, _mm_mul_ps(src_ps, coeff));
+                        }
+
+                        coeff_ptr += coeff[i]->coeff_stride;
+                        src_ptr += src_stride;
+                    }
+
+                    dstp[x] = _mm_cvtss_f32(_mm_hadd_ps(_mm_hadd_ps(result, result), _mm_hadd_ps(result, result)));
+                }
+            }
+        }
+    }
+}
+
+template void JincResize::resize_plane_sse41<uint8_t>(EWAPixelCoeff* coeff[3], PVideoFrame& src, PVideoFrame& dst, IScriptEnvironment* env);
+template void JincResize::resize_plane_sse41<uint16_t>(EWAPixelCoeff* coeff[3], PVideoFrame& src, PVideoFrame& dst, IScriptEnvironment* env);
+template void JincResize::resize_plane_sse41<float>(EWAPixelCoeff* coeff[3], PVideoFrame& src, PVideoFrame& dst, IScriptEnvironment* env);
