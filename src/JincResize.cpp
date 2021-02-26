2e1267358606b16a603c3bbb8eb62309f6fc0fe4
DTL2020 <68707763+DTL2020@users.noreply.github.com>
2021-02-26 22:07:40 +0300

    Started add L1d cache optim
    Added _nvs functions for C and AVX2 for test performance with large multithreading. Got about 2.5 times slower in compare with _cb at AVX2 and close speed at C. Need much more further think and test how to improve data locality (actually cb buf) to L1d cache and also memory and cache management.

--- src/JincResize.cpp
+++ src/JincResize.cpp
@@ -773,9 +773,10 @@ JincResize::JincResize(PClip _child, int target_width, int target_height, double
                 {
                     if (threads_ == 1)
                         //KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb; 
-                        KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_2r; // - to be debugged
+//                        KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_2r; // - to be debugged
+                        KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt_nvs;
                     else
-                        KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt;
+                        KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt_nvs;
                         //KernelProcAll_MTK = &JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt_2r; //-- a bit faster, need to debug later
                 }
                 else                
@@ -858,7 +859,8 @@ JincResize::JincResize(PClip _child, int target_width, int target_height, double
 			}
 			else // ap == 2
 //				KernelProcAll = &JincResize::KernelRowAll_c_mul_cb_mt; // faster with iC++ in compare with _nz and _frw
-				KernelProcAll = &JincResize::KernelRowAll_c_mul_cb_2r;
+				//KernelProcAll = &JincResize::KernelRowAll_c_mul_cb_2r;
+                KernelProcAll = &JincResize::KernelRowAll_c_mul_cb_mt_nvs;
 
         switch (vi.ComponentSize())
         {
@@ -1035,6 +1037,41 @@ void JincResize::ConvertNRowsToInt_c(std::vector<float*>const& Vector, int iInpW
 	}
 }
 
+void JincResize::ConvertNRowsNColsToInt_c(std::vector<float*>const& Vector, int iInpWidth, int iOutStartRow, unsigned char* dst, const int iDstStride, const int iNumRows, const int iStartCol, const int iNumCols, const int iDstWidth)
+{
+    int row_float_buf_index = 0;
+    int iShift = iTaps*iMul;// (iKernelSize + iTaps)* iMul;
+    int iEndCol = iStartCol + iNumCols; 
+    if (iEndCol > iDstWidth) iEndCol = iDstWidth - 1;
+
+    for (int row = iOutStartRow; row < iOutStartRow + iNumRows; row++)
+    {
+        float* pfSrc = Vector[row_float_buf_index];
+        for (int col = iStartCol; col < iEndCol; col++)
+        {
+            unsigned char ucVal;
+            float* pfCurrVal = pfSrc + col + iShift;
+//            float fVal = pfSrc[col + iShift];
+            float fVal = *pfCurrVal;
+
+            fVal += 0.5f;
+
+            if (fVal > 255.0f)
+            {
+                fVal = 255.0f;
+            }
+            if (fVal < 0.0f)
+            {
+                fVal = 0.0f;
+            }
+            ucVal = (unsigned char)fVal;
+
+            dst[(row * iDstStride + col)] = ucVal;
+        }
+        row_float_buf_index++;
+    }
+}
+
 void JincResize::GetInpElRowAsFloat_c(int iInpRow, int iCurrInpHeight, int iCurrInpWidth,  unsigned char* pCurr_src, int iCurrSrcStride, float* dst)
 {
     int64_t col;
@@ -1057,6 +1094,38 @@ void JincResize::GetInpElRowAsFloat_c(int iInpRow, int iCurrInpHeight, int iCurr
     for (col = iKernelSize + iCurrInpWidth; col < iWidthEl; col++) // right
     {
         dst[col] = (float)pCurr_src[((iInpRow - iKernelSize) * iCurrSrcStride) + (iCurrInpWidth - 1)];
+    }
+}
+
+void JincResize::GetInpElNColsAsFloat_c(int iInpRow, int iCurrInpHeight, int iCurrInpWidth, unsigned char* pCurr_src, int iCurrSrcStride, float* dst, int iColStart, int iNumCols)
+{
+    int64_t col;
+    // row range from iTaps to iHeightEl - iTaps - 1
+
+    if (iInpRow < iKernelSize) iInpRow = iKernelSize;
+    if (iInpRow > (iCurrInpHeight + iKernelSize - 1)) iInpRow = iCurrInpHeight + iKernelSize - 1;
+
+    // start cols
+    for (col = iColStart; col < iKernelSize; col++) // left
+    {
+        dst[col] = (float)pCurr_src[((iInpRow - iKernelSize) * iCurrSrcStride) + 0];
+
+        iNumCols--;
+        if (iNumCols == 0) return; // may be slow but so for now. TO DO - rewrite
+    }
+    // mid cols
+    for (; col < iKernelSize + iCurrInpWidth; col++) // left
+    {
+        dst[col] = (float)pCurr_src[((iInpRow - iKernelSize) * iCurrSrcStride) + (col - iKernelSize)];
+        iNumCols--;
+        if (iNumCols == 0) return;
+    }
+    // end cols
+    for (; col < iWidthEl; col++) // right
+    {
+        dst[col] = (float)pCurr_src[((iInpRow - iKernelSize) * iCurrSrcStride) + (iCurrInpWidth - 1)];
+        iNumCols--;
+        if (iNumCols == 0) return;
     }
 }
 
@@ -1317,6 +1386,95 @@ void JincResize::KernelRowAll_c_mul_cb_mt(unsigned char *src, int iSrcStride, in
 	} // parallel section
 }
 
+/* circulating buf narrow vertical strips scan version
+size of used circ buf ~half of each core L1d cache size
+*/
+void JincResize::KernelRowAll_c_mul_cb_mt_nvs(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride)
+{
+    iWidthEl = iInpWidth + 2 * iKernelSize;
+    iHeightEl = iInpHeight + 2 * iKernelSize;
+
+    const int64_t iNumRowsPerThread = (iHeightEl - 2 * iTaps) / threads_;
+
+    const int64_t iNumColumnsPerVStrip = 32; //  (iHeightEl - 2 * iTaps) / threads_; - temp 32 for full hd to 8k test
+    const int64_t iNumVStrips = iWidthEl / iNumColumnsPerVStrip;
+
+    // initial clearing
+    for (int j = 0; j < threads_; j++)
+    {
+        for (int i = 0; i < iKernelSize; i++)
+        {
+            memset(vpvThreadsVectors[j][i], 0, iWidthEl * iMul * sizeof(float));
+        }
+    }
+
+#pragma omp parallel num_threads(threads_)
+    {
+        // start all thread dependent ptrs here
+        int64_t tidx = omp_get_thread_num(); // our thread id here
+        float* pfInpRowSamplesFloatBufStart = pfInpFloatRow + tidx * iWidthEl;
+
+        std::vector<float*> vpfThreadVector = vpvThreadsVectors[tidx];
+        // it looks vpfThreadVector uses copy of vector from vpvThreadsVectors and it may be slower, TO DO - remake to pointer to vector
+
+        for (int64_t iVStrip = 0; iVStrip < iNumVStrips - 1; iVStrip++)
+        {
+            int64_t iStartCol = iVStrip * iNumColumnsPerVStrip;
+            int64_t iEndCol = iStartCol + iNumColumnsPerVStrip;
+
+            if (iEndCol > iWidthEl - iTaps) iEndCol = iWidthEl - iTaps;
+
+            // calculate rows to process in this thread
+            int64_t iStartRow = tidx * iNumRowsPerThread;
+            int64_t iThreadSkipRows = iTaps * 2;
+            // some check
+            if (iStartRow < iTaps) iStartRow = iTaps;
+            int64_t iEndRow = iStartRow + iNumRowsPerThread + 2 * iTaps;
+            if (iEndRow > iHeightEl - iTaps) iEndRow = iHeightEl - iTaps;
+
+            for (int64_t row = iStartRow; row < iEndRow; row++)
+            {
+                GetInpElNColsAsFloat_c(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart, iStartCol, iNumColumnsPerVStrip);
+
+                for (int64_t col = /*iTaps*/ + iStartCol; col < iEndCol/*iWidthEl - iTaps*/; col++) // input cols counter
+                {
+                    float fInpSample = pfInpRowSamplesFloatBufStart[col];
+                    float* pfCurrKernel_pos = g_pfKernel;
+                    float* pfProc;
+
+                    for (int64_t k_row = 0; k_row < iKernelSize; k_row++)
+                    {
+                        pfProc = vpfThreadVector[k_row] + col * iMul;
+                        for (int64_t k_col = 0; k_col < iKernelSize; k_col++)
+                        {
+                            pfProc[k_col] += pfCurrKernel_pos[k_col] * fInpSample;
+                        } // k_col 
+                        pfCurrKernel_pos += iKernelSize; // point to next kernel row now
+                    } // k_row
+
+                } // col
+
+                int iOutStartRow = (row - (iTaps + iKernelSize)) * iMul;
+                //iMul rows ready - output result, skip iKernelSize+iTaps rows from beginning
+                if (iOutStartRow >= 0 && iOutStartRow < (iInpHeight)*iMul && iThreadSkipRows <= 0)
+                {
+                     ConvertNRowsNColsToInt_c(vpfThreadVector, iInpWidth, iOutStartRow, dst, iDstStride, iMul, iStartCol * iMul, iNumColumnsPerVStrip * iMul, iInpWidth * iMul);
+                }
+
+                iThreadSkipRows--;
+
+                // circulate pointers to iMul rows upper
+                std::rotate(vpfThreadVector.begin(), vpfThreadVector.begin() + iMul, vpfThreadVector.end());
+
+                // clear last iMul rows
+                for (int i = iKernelSize - iMul; i < iKernelSize; i++)
+                {
+                    memset(vpfThreadVector[i], 0, iWidthEl * iMul * sizeof(float));
+                }
+            } // row
+        } // iVStrip
+    } // parallel section
+}
 
 void JincResize::KernelRowAll_c_mul_cb_is(unsigned char *src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char *dst, int iDstStride)
 {
--- src/KernelRow_avx2.cpp
+++ src/KernelRow_avx2.cpp
@@ -501,7 +501,7 @@ void JincResize::KernelRowAll_avx2_mul4_taps4_cb_2r(unsigned char* src, int iSrc
     memset(pfFilteredCirculatingBuf, 0, iWidthEl * (32+4/*iKernelSize + iMul*/) * 4/*iMul*/ * sizeof(float));
 
     // no MT version 
-    for (int64_t row = 4/*iTaps*/; row < (iHeightEl - 8/*iTaps*/); row += 2) // input lines counter  !!! temp -8 because of 2 rows proc ?
+    for (int64_t row = 4/*iTaps*/; row < (iHeightEl - 8/*iTaps*2*/); row += 2) // input lines counter  !!! temp -8 because of 2 rows proc ?
     {
         // start all row-only dependent ptrs here 
         float* pfInpRowSamplesFloatBufStart_r1 = pfInpFloatRow;
@@ -1143,6 +1143,223 @@ void JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt(unsigned char* src, int iSrc
                 memset(vpfThreadVector[i], 0, iWidthEl * ciMul * sizeof(float));
             }
         } // row
+    } // parallel section
+}
+
+void JincResize::KernelRowAll_avx2_mul4_taps4_cb_mt_nvs(unsigned char* src, int iSrcStride, int iInpWidth, int iInpHeight, unsigned char* dst, int iDstStride, const int ciMul, const int ciTaps, const int ciKS)
+{
+
+    // current input plane sizes
+    iWidthEl = iInpWidth + 2 * ciKS;
+    iHeightEl = iInpHeight + 2 * ciKS;
+
+    const int64_t iNumColumnsPerVStrip = 32; //  (iHeightEl - 2 * iTaps) / threads_; - temp 32 for full hd to 8k test
+    const int64_t iNumVStrips = iWidthEl / iNumColumnsPerVStrip;
+
+    const int k_col8 = ciKS - (ciKS % 8);
+    const int col8 = iWidthEl - ciTaps - ((iWidthEl - ciTaps) % 8);
+    float* pfCurrKernel = g_pfKernel;
+
+    const int64_t iNumRowsPerThread = (iHeightEl - 2 * ciTaps) / threads_;
+
+    // initial clearing
+    for (int j = 0; j < threads_; j++)
+    {
+        for (int i = 0; i < iKernelSize; i++)
+        {
+            memset(vpvThreadsVectors[j][i], 0, iWidthEl * ciMul * sizeof(float));
+        }
+    }
+
+#pragma omp parallel num_threads(threads_)
+    {
+        int64_t tidx = omp_get_thread_num(); // our thread id here
+
+        std::vector<float*> vpfThreadVector = vpvThreadsVectors[tidx];
+        // it looks vpfThreadVector uses copy of vector from vpvThreadsVectors and it may be slower, TO DO - remake to pointer to vector
+
+        for (int64_t iVStrip = 0; iVStrip < iNumVStrips - 1; iVStrip++)
+        {
+            int64_t iStartCol = iVStrip * iNumColumnsPerVStrip;
+            int64_t iEndCol = iStartCol + iNumColumnsPerVStrip;
+
+            if (iEndCol > iWidthEl - iTaps) iEndCol = iWidthEl - iTaps;
+
+            // calculate rows to process in this thread
+            int64_t iStartRow = tidx * iNumRowsPerThread;
+            int64_t iThreadSkipRows = 2 * ciTaps;//  iTaps * 2;
+            // some check
+            if (iStartRow < ciTaps) iStartRow = ciTaps;// iTaps;
+            int64_t iEndRow = iStartRow + iNumRowsPerThread + 2 * ciTaps;// +2 * iTaps;
+            if (iEndRow > iHeightEl - ciTaps) iEndRow = iHeightEl - ciTaps;//  -iTaps;
+            // prepare float32 pre-converted row data for each threal separately
+
+            for (int64_t row = iStartRow; row < iEndRow; row++)
+            {
+
+                float* pfInpRowSamplesFloatBufStart = pfInpFloatRow + tidx * iWidthEl;
+//                GetInpElRowAsFloat_avx2(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart, ciKS);
+                GetInpElNColsAsFloat_c(row, iInpHeight, iInpWidth, src, iSrcStride, pfInpRowSamplesFloatBufStart, iStartCol, iNumColumnsPerVStrip);
+
+                int64_t col;
+
+                for (col = iStartCol/*ciTaps*/; col < iEndCol/*col8*/; col += 8) // input cols counter iTaps
+                {
+                    float* pfColStart = pfInpRowSamplesFloatBufStart + col;
+
+                    float* pfCurrKernel_pos = pfCurrKernel;
+
+                    for (int64_t k_row = 0; k_row < ciKS; k_row++)
+                    {
+                        float* pfProc = vpfThreadVector[k_row] + col * ciMul;// *iMul;
+
+                        __m256 my_ymm0, my_ymm1, my_ymm2, my_ymm3, my_ymm4, my_ymm5, my_ymm6, my_ymm7; // out samples
+                        __m256 my_ymm8, my_ymm9, my_ymm10, my_ymm11; // inp samples
+                        __m256 my_ymm12, my_ymm13, my_ymm14, my_ymm15; // kernel samples
+
+                        my_ymm12 = _mm256_load_ps(pfCurrKernel_pos);
+                        my_ymm13 = _mm256_load_ps(pfCurrKernel_pos + 8);
+                        my_ymm14 = _mm256_load_ps(pfCurrKernel_pos + 16);
+                        my_ymm15 = _mm256_load_ps(pfCurrKernel_pos + 24);
+
+                        my_ymm0 = _mm256_load_ps(pfProc);
+                        my_ymm1 = _mm256_load_ps(pfProc + 8);
+                        my_ymm2 = _mm256_load_ps(pfProc + 16);
+                        my_ymm3 = _mm256_load_ps(pfProc + 24);
+                        my_ymm4 = _mm256_load_ps(pfProc + 32);
+                        my_ymm5 = _mm256_load_ps(pfProc + 40);
+                        my_ymm6 = _mm256_load_ps(pfProc + 48);
+
+                        my_ymm8 = _mm256_broadcast_ss(pfColStart + 0); // 1
+                        my_ymm9 = _mm256_broadcast_ss(pfColStart + 2); // 3
+                        my_ymm10 = _mm256_broadcast_ss(pfColStart + 4); // 5
+                        my_ymm11 = _mm256_broadcast_ss(pfColStart + 6); // 7 
+
+                            // 1st sample
+                        my_ymm0 = _mm256_fmadd_ps(my_ymm12, my_ymm8, my_ymm0);
+                        my_ymm1 = _mm256_fmadd_ps(my_ymm13, my_ymm8, my_ymm1);
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm14, my_ymm8, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm15, my_ymm8, my_ymm3);
+
+                        // 3rd sample
+                        my_ymm1 = _mm256_fmadd_ps(my_ymm12, my_ymm9, my_ymm1);
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm13, my_ymm9, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm14, my_ymm9, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm15, my_ymm9, my_ymm4);
+
+                        // 5th sample
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm12, my_ymm10, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm13, my_ymm10, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm14, my_ymm10, my_ymm4);
+                        my_ymm5 = _mm256_fmadd_ps(my_ymm15, my_ymm10, my_ymm5);
+
+
+                        // 7th sample
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm12, my_ymm11, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm13, my_ymm11, my_ymm4);
+                        my_ymm5 = _mm256_fmadd_ps(my_ymm14, my_ymm11, my_ymm5);
+                        my_ymm6 = _mm256_fmadd_ps(my_ymm15, my_ymm11, my_ymm6);
+
+
+                        _mm_store_ps(pfProc, _mm256_castps256_ps128(my_ymm0));
+                        my_ymm0 = _mm256_permute2f128_ps(my_ymm0, my_ymm1, 33);
+                        my_ymm1 = _mm256_permute2f128_ps(my_ymm1, my_ymm2, 33);
+                        my_ymm2 = _mm256_permute2f128_ps(my_ymm2, my_ymm3, 33);
+                        my_ymm3 = _mm256_permute2f128_ps(my_ymm3, my_ymm4, 33);
+                        my_ymm4 = _mm256_permute2f128_ps(my_ymm4, my_ymm5, 33);
+                        my_ymm5 = _mm256_permute2f128_ps(my_ymm5, my_ymm6, 33);
+                        my_ymm6 = _mm256_permute2f128_ps(my_ymm6, my_ymm6, 49);
+                        my_ymm6 = _mm256_insertf128_ps(my_ymm6, *(__m128*)(pfProc + 56), 1);
+
+                        // even samples
+                        my_ymm8 = _mm256_broadcast_ss(pfColStart + 1); // 2
+                        my_ymm9 = _mm256_broadcast_ss(pfColStart + 3); // 4
+                        my_ymm10 = _mm256_broadcast_ss(pfColStart + 5); // 6
+                        my_ymm11 = _mm256_broadcast_ss(pfColStart + 7); // 8 
+
+                            // 2nd sample
+                        my_ymm0 = _mm256_fmadd_ps(my_ymm12, my_ymm8, my_ymm0);
+                        my_ymm1 = _mm256_fmadd_ps(my_ymm13, my_ymm8, my_ymm1);
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm14, my_ymm8, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm15, my_ymm8, my_ymm3);
+
+                        // 4th sample
+                        my_ymm1 = _mm256_fmadd_ps(my_ymm12, my_ymm9, my_ymm1);
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm13, my_ymm9, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm14, my_ymm9, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm15, my_ymm9, my_ymm4);
+
+                        // 6th sample
+                        my_ymm2 = _mm256_fmadd_ps(my_ymm12, my_ymm10, my_ymm2);
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm13, my_ymm10, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm14, my_ymm10, my_ymm4);
+                        my_ymm5 = _mm256_fmadd_ps(my_ymm15, my_ymm10, my_ymm5);
+
+                        // 7th sample
+                        my_ymm3 = _mm256_fmadd_ps(my_ymm12, my_ymm11, my_ymm3);
+                        my_ymm4 = _mm256_fmadd_ps(my_ymm13, my_ymm11, my_ymm4);
+                        my_ymm5 = _mm256_fmadd_ps(my_ymm14, my_ymm11, my_ymm5);
+                        my_ymm6 = _mm256_fmadd_ps(my_ymm15, my_ymm11, my_ymm6);
+
+                        _mm256_store_ps(pfProc + 4, my_ymm0);
+                        _mm256_store_ps(pfProc + 12, my_ymm1);
+                        _mm256_store_ps(pfProc + 20, my_ymm2);
+                        _mm256_store_ps(pfProc + 28, my_ymm3);
+                        _mm256_store_ps(pfProc + 36, my_ymm4);
+                        _mm256_store_ps(pfProc + 44, my_ymm5);
+                        _mm256_store_ps(pfProc + 52, my_ymm6);
+
+                        pfCurrKernel_pos += ciKS;// +=iKernelSize; // point to next kernel row now
+                    } // k_row
+
+                } // col
+                /*
+                // need to process last up to 7 cols separately...
+                for (col = col8 + ciTaps; col < iWidthEl - ciTaps; col++) // input cols counter +iT - iT
+                {
+                    float* pfCurrKernel_pos = pfCurrKernel;
+                    float* pfProc;
+
+                    for (int64_t k_row = 0; k_row < ciKS; k_row++) // iKS
+                    {
+                        pfProc = vpfThreadVector[k_row] + col * ciMul; // *iMul;
+                        for (int64_t k_col = 0; k_col < k_col8; k_col += 8)
+                        {
+                            *(__m256*)(pfProc + k_col) = _mm256_fmadd_ps(*(__m256*)(pfCurrKernel_pos + k_col), _mm256_broadcast_ss(&pfInpRowSamplesFloatBufStart[col]), *(__m256*)(pfProc + k_col));
+                        }
+
+                        // need to process last (up to 7) floats separately..
+                        for (int64_t k_col = k_col8; k_col < ciKS; ++k_col) // iKS
+                        {
+                            pfProc[k_col] += (pfCurrKernel_pos[k_col] * pfInpRowSamplesFloatBufStart[col]);
+                        }
+                        pfCurrKernel_pos += ciKS;// +=iKernelSize; // point to next kernel row now
+                    } // k_row
+
+                } // col
+                */
+
+                int iOutStartRow = (row - (ciTaps + ciKS)) * ciMul;
+                //iMul rows ready - output result, skip iKernelSize+iTaps rows from beginning
+                if (iOutStartRow >= 0 && iOutStartRow < (iInpHeight)*ciMul && iThreadSkipRows <= 0)
+                {
+                    // it looks vpfThreadVector uses copy of vector and it may be slower, TO DO - remake to pointer to vector
+//                    ConvertNRowsToInt_avx2(vpfThreadVector, iInpWidth, iOutStartRow, dst, iDstStride, ciMul);
+                    ConvertNRowsNColsToInt_avx2(vpfThreadVector, iInpWidth, iOutStartRow, dst, iDstStride, ciMul, iStartCol* ciMul, iNumColumnsPerVStrip* ciMul, iInpWidth* ciMul);
+                }
+
+                iThreadSkipRows--;
+
+                // circulate pointers to iMul rows upper
+                std::rotate(vpfThreadVector.begin(), vpfThreadVector.begin() + ciMul, vpfThreadVector.end());
+
+                // clear last iMul rows
+                for (int i = ciKS - ciMul; i < ciKS; i++)
+                {
+                    memset(vpfThreadVector[i], 0, iWidthEl * ciMul * sizeof(float));
+                }
+            } // row
+        } // iVStrip
     } // parallel section
 }
 
@@ -2449,6 +2666,81 @@ __forceinline void JincResize::ConvertNRowsToInt_avx2(std::vector<float*>const& 
             pucDst++;
             pfProc++;
         } //l_col
+
+        row_float_buf_index++;
+    } // row
+}
+
+__forceinline void JincResize::ConvertNRowsNColsToInt_avx2(std::vector<float*>const& Vector, int iInpWidth, int iOutStartRow, unsigned char* dst, int iDstStride, int iNumRows, const int iStartCol, const int iNumCols, const int iDstWidth)
+{
+    const int col32 = (iInpWidth * iMul) - ((iInpWidth * iMul) % 32);
+
+    int iEndCol = iStartCol + iNumCols;
+    if (iEndCol > iDstWidth) iEndCol = iDstWidth - 1;
+
+    __m256 my_zero_ymm2;
+
+    int row_float_buf_index = 0;
+
+    for (int64_t row = iOutStartRow; row < iOutStartRow + iNumRows; row++)
+    {
+        my_zero_ymm2 = _mm256_setzero_ps();
+
+        int64_t col = 0;
+        float* pfProc = Vector[row_float_buf_index] + iTaps * iMul/*(iKernelSize + iTaps) * iMul*/ + iStartCol;
+        unsigned char* pucDst = dst + row * (int64_t)iDstStride + iStartCol;
+
+        for (col = iStartCol/*0*/; col < iEndCol/*col32*/; col += 32)
+        {
+            __m256 my_Val_ymm1 = _mm256_load_ps(pfProc);
+            __m256 my_Val_ymm2 = _mm256_load_ps(pfProc + 8);
+            __m256 my_Val_ymm3 = _mm256_load_ps(pfProc + 16);
+            __m256 my_Val_ymm4 = _mm256_load_ps(pfProc + 24);
+
+            my_Val_ymm1 = _mm256_max_ps(my_Val_ymm1, my_zero_ymm2);
+            my_Val_ymm2 = _mm256_max_ps(my_Val_ymm2, my_zero_ymm2);
+            my_Val_ymm3 = _mm256_max_ps(my_Val_ymm3, my_zero_ymm2);
+            my_Val_ymm4 = _mm256_max_ps(my_Val_ymm4, my_zero_ymm2);
+
+            __m256i my_iVal_ymm1 = _mm256_cvtps_epi32(my_Val_ymm1);
+            __m256i my_iVal_ymm2 = _mm256_cvtps_epi32(my_Val_ymm2);
+            __m256i my_iVal_ymm3 = _mm256_cvtps_epi32(my_Val_ymm3);
+            __m256i my_iVal_ymm4 = _mm256_cvtps_epi32(my_Val_ymm4);
+
+            __m256i my_iVal_12 = _mm256_packus_epi32(my_iVal_ymm1, my_iVal_ymm2);
+            __m256i my_iVal_34 = _mm256_packus_epi32(my_iVal_ymm3, my_iVal_ymm4);
+
+            __m256i my_iVal_1234 = _mm256_packus_epi16(my_iVal_12, my_iVal_34);
+
+            __m256i my_iVal_ymm1234 = _mm256_permutevar8x32_epi32(my_iVal_1234, _mm256_setr_epi32(0, 4, 1, 5, 2, 6, 3, 7));
+
+            _mm256_stream_si256((__m256i*)(pucDst), my_iVal_ymm1234);
+
+            pucDst += 32;
+            pfProc += 32;
+        }
+  /*      // last up to 31..
+        for (int64_t l_col = col32; l_col < iInpWidth * iMul; ++l_col)
+        {
+
+            float fVal = *pfProc;
+
+            fVal += 0.5f;
+
+            if (fVal > 255.0f)
+            {
+                fVal = 255.0f;
+            }
+            if (fVal < 0.0f)
+            {
+                fVal = 0.0f;
+            }
+
+            *pucDst = (unsigned char)fVal;
+
+            pucDst++;
+            pfProc++;
+        } //l_col */
 
         row_float_buf_index++;
     } // row
