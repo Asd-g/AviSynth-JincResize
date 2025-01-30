95f9e19d5914a3222cedae19af63c24d70361709
DTL2020 <68707763+DTL2020@users.noreply.github.com>
2025-01-30 18:48:13 +0300

    Added error throwing at
    bad aligned memory allocation.

--- src/JincResize.cpp
+++ src/JincResize.cpp
@@ -471,6 +471,8 @@ static void generate_coeff_table_c(Lut* func, EWAPixelCoeff* out, int quantize_x
     out->factor = static_cast<float*>(aligned_malloc(aligned_size, 64)); // aligned to cache line
     if (out->factor)
         memcpy(out->factor, &tmp_array[0], tmp_array_size * sizeof(float));
+    else
+        throw std::invalid_argument("Can not allocate memory");
 }
 
 /* Planar resampling with coeff table */
